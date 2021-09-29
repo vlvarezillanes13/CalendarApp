8b61d28a3ddf34470c4760164599628020a643a4
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2021-09-29 16:09:55 -0300

    CALENDAR && EVENT MODAL

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,88 @@
-import React from 'react'
-import { Navbar } from '../ui/Navbar'
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'moment/locale/es';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import { messages } from '../../helpers/calendar-messages-es';
+import { Navbar } from '../ui/Navbar';
+import { CalendarEvent } from './CalendarEvent';
+import { CalendarModal } from './CalendarModal';
+
+
+moment.locale('es');
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title:'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add(2,'hours').toDate(),
+    bgcolor: '#fafafa',
+    user:{
+        _id:'123',
+        name:'Vicente'
+    }
+}]
+
 
 export const CalendarScreen = () => {
+
+    const [lastView, setlastView] = useState(localStorage.getItem('lasView') || 'month');
+
+    const onDoubleClick = () => {
+
+    };
+
+    const onSelectEvent = () => {
+
+    };
+
+
+    const onViewChange = (e) => {
+        setlastView(e);
+        localStorage.setItem('lasView',e);
+    };
+
+
+    const eventStyleGetter = ( event, start, end, inSelected ) => {
+
+        const style = {
+
+            backgroundColor:'#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+        return{
+            style
+        }
+    };
+
+
     return (
-        <div>
+        <div className='calendar-screen'>
             <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={ events }
+                startAccessor="start"
+                endAccessor="end"
+                messages = { messages }
+                eventPropGetter = { eventStyleGetter }
+                onDoubleClickEvent = { onDoubleClick }
+                onSelectEvent = { onSelectEvent }
+                onView = { onViewChange }
+                view = { lastView }
+                components = {{
+                    event: CalendarEvent,
+                }}
+            />
+
+
+            <CalendarModal />
         </div>
     )
 }
